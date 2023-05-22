e37737ed9ea637172ba0714be0909ebfd2f5cba1
Sergio Mata <sergiomataherrero@gmail.com>
2023-05-22 18:00:17 +0200

    Add setup settings to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
 import { Header } from "./components/header";
 
 function App() {
   return (
     <>
       <div>
-        <Header title="Challenge 1" subtitle="Week 4"></Header>
+        <Header title="Pacones" subtitle="pointing at you"></Header>
       </div>
     </>
   );
