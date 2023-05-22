20050b19040d810b64ce298147467438187e39f0
Sergio Mata <sergiomataherrero@gmail.com>
2023-05-22 17:04:17 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Header } from "./components/header";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header title="Challenge 1" subtitle="Week 4"></Header>
+      </div>
+    </>
+  );
+}
+
+export default App;
