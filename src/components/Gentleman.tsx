1bcdceff8fbe616902b4f110df68161a49e7f0e1
Sergio Mata <sergiomataherrero@gmail.com>
2023-05-22 20:30:05 +0200

    Update layout with Gentleman component

--- src/components/Gentleman.tsx
+++ src/components/Gentleman.tsx
@@ -1,99 +1,41 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { gentlemen } from "../data/gentlemen.mock";
 
 export function Gentleman() {
   return (
     <>
-      {gentlemen.map((item) => (
-        <li className="gentleman" key={item.id}>
-          <div className="gentlemanavatar-container">
+      {gentlemen.map((pacon) => (
+        <li key={pacon.id} className="gentleman">
+          <div className="gentleman__avatar-container">
             <img
-              className="gentlemanavatar"
-              src={item.picture}
-              alt={item.alternativeText}
+              className="gentleman__avatar"
+              src={pacon.picture}
+              alt={pacon.alternativeText}
             />
-            <span className="gentlemaninitial">
-              {item.name.charAt(0).toUpperCase()}
+            <span className="gentleman__initial">
+              {pacon.name.charAt(0).toUpperCase()}
             </span>
           </div>
-          <div className="gentlemandata-container">
-            <h2 className="gentlemanname">{item.name}</h2>
-            <ul className="gentlemandata-list">
-              <li className="gentlemandata">
-                <span className="gentlemandata-label">
-                  Profession: {item.profession}
-                </span>
-                Influencer
+          <div className="gentleman__data-container">
+            <h2 className="gentleman__name">{pacon.name}</h2>
+            <ul className="gentleman__data-list">
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Profession:</span>
+                {pacon.profession}
               </li>
-              <li className="gentlemandata">
-                <span className="gentlemandata-label">
-                  Status: {item.status}
-                </span>
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Status:</span>
+                {pacon.status}
               </li>
-              <li className="gentlemandata">
-                <span className="gentlemandata-label">
-                  Twitter: {item.twitter}
-                </span>
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Twitter:</span>
+                {pacon.twitter}
               </li>
             </ul>
           </div>
-          <i className="icon gentlemanicon fas fa-check"></i>
-          <i className="icon gentlemanicon gentleman__icon--delete fas fa-times"></i>
+          <i className="icon gentleman__icon fas fa-check"></i>
+          <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
         </li>
       ))}
     </>
   );
 }
-
-// import { gentlemen } from "../data/gentlemen.mock";
-
-// // type GentlemenStructure = {
-// //   id: number;
-// //   name: string;
-// //   status: string;
-// //   profession: string;
-// //   twitter: string;
-// //   picture: string;
-// //   alternativeText: string;
-// //   selected: boolean;
-// // };
-
-// export function Gentleman() {
-//   return (
-//     <>
-//       {gentlemen.map((item) => {
-//         <li key={item.id} className="gentleman">
-//           <div className="gentleman__avatar-container">
-//             <img
-//               className="gentleman__avatar"
-//               src={item.picture}
-//               alt={item.alternativeText}
-//             />
-//             <span className="gentleman__initial">
-//               {item.name.charAt(0).toUpperCase()}
-//             </span>
-//           </div>
-//           <div className="gentleman__data-container">
-//             <h2 className="gentleman__name">{item.name}</h2>
-//             <ul className="gentleman__data-list">
-//               <li className="gentleman__data">
-//                 <span className="gentleman__data-label">Profession:</span>
-//                 {item.profession}
-//               </li>
-//               <li className="gentleman__data">
-//                 <span className="gentleman__data-label">Status:</span>{" "}
-//                 {item.status}
-//               </li>
-//               <li className="gentleman__data">
-//                 <span className="gentleman__data-label">Twitter:</span>{" "}
-//                 {item.twitter}
-//               </li>
-//             </ul>
-//           </div>
-//           <i className="icon gentleman__icon fas fa-check"></i>
-//           <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
-//         </li>;
-//       })}
-//     </>
-//   );
-// }
